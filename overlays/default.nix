becc9168ccf37034ae3e25db9e6dc5c405eabfef
Angel J <78835633+iamanaws@users.noreply.github.com>
2026-05-12 20:26:59 -0700

    overlays: patch jitterentropy-rngd mlock issue

--- overlays/default.nix
+++ overlays/default.nix
@@ -12,6 +12,16 @@
   # You can change versions, add patches, set compilation flags, anything really.
   # https://nixos.wiki/wiki/Overlays
   modifications = final: prev: {
+    # TODO: remove once jitterentropy-rngd 1.3.1's systemd unit fix lands in nixpkgs.
+    jitterentropy-rngd = prev.jitterentropy-rngd.overrideAttrs (oldAttrs: {
+      postPatch = ''
+        ${oldAttrs.postPatch or ""}
+        substituteInPlace jitterentropy.service.in \
+          --replace-fail "LimitMEMLOCK=0" "LimitMEMLOCK=2M" \
+          --replace-fail "mincore mlock mlockall personality" "mincore mlockall personality"
+      '';
+    });
+
     # example = prev.example.overrideAttrs (oldAttrs: rec {
     # ...
     # });
