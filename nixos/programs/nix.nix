14b50d0a06f363487ba8cbc4441df49fd2915898
Angel J <78835633+iamanaws@users.noreply.github.com>
2026-05-17 19:05:08 -0700

    nixos: add spectrum cache

--- nixos/programs/nix.nix
+++ nixos/programs/nix.nix
@@ -22,12 +22,14 @@
 
       substituters = [
         "https://cache.nixos.org/"
+        "https://cache.dataaturservice.se/spectrum/"
         # "https://nix-community.cachix.org"
         # "https://nix-gaming.cachix.org"
       ];
 
       trusted-public-keys = [
         "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+        "spectrum-os.org-2:foQk3r7t2VpRx92CaXb5ROyy/NBdRJQG2uX2XJMYZfU="
         # "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
         # "nix-gaming.cachix.org-1:nbjlureqMbRAxR1gJ/f3hxemL9svXaZF/Ees8vCUUs4="
       ];
