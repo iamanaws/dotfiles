6221409eac03dd2b54d81537ef12d99860111d2f
Angel J <78835633+Iamanaws@users.noreply.github.com>
2026-05-22 17:33:17 -0700

    flake.lock: update

--- overlays/default.nix
+++ overlays/default.nix
@@ -11,15 +11,16 @@
   # You can change versions, add patches, set compilation flags, anything really.
   # https://nixos.wiki/wiki/Overlays
   modifications = final: prev: {
-    # TODO: remove once jitterentropy-rngd 1.3.1's systemd unit fix lands in nixpkgs.
-    jitterentropy-rngd = prev.jitterentropy-rngd.overrideAttrs (oldAttrs: {
-      postPatch = ''
-        ${oldAttrs.postPatch or ""}
-        substituteInPlace jitterentropy.service.in \
-          --replace-fail "LimitMEMLOCK=0" "LimitMEMLOCK=2M" \
-          --replace-fail "mincore mlock mlockall personality" "mincore mlockall personality"
-      '';
-    });
+    # remove when reaching nixos-unstable https://nixpk.gs/pr-tracker.html?pr=522705
+    pythonPackagesExtensions = (prev.pythonPackagesExtensions or [ ]) ++ [
+      (
+        _pythonFinal: pythonPrev: {
+          jedi-language-server = pythonPrev.jedi-language-server.overridePythonAttrs (_oldAttrs: {
+            pythonRelaxDeps = [ "jedi" ];
+          });
+        }
+      )
+    ];
 
     # example = prev.example.overrideAttrs (oldAttrs: rec {
     # ...
